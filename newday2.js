f3a50279f5ed8532b3916ac86ebb4fb9fb2191d8
Martin <martinjlangley@gmail.com>
2022-02-18 17:51:47 +0000

    update on date day 2 activities file

---
+++ newday2.js
@@ -0,0 +1,23 @@
+const string = 'The revolution will not be televised.';
+console.log(string);    
+
+const bigmouth = 'I\'ve got no right to take my place...';
+console.log(bigmouth);        
+
+const myString = '123';
+const myNum = Number(myString);
+console.log(typeof myNum);
+
+const myNum2 = 123;
+const myString2 = myNum2.toString();
+console.log(typeof myString2);
+
+
+// const greeting = "Hello";
+// const name = "Martin";
+// console.log(greeting + ", " + name);
+
+const greeting = "Hello";
+const name = "Chris";
+console.log(`${greeting}, ${name}`);
+
