293b39c5bb16b64472a22488bb728f9649b31e8e
Martin <martinjlangley@gmail.com>
2022-02-18 16:24:38 +0000

    First commited1624

---
+++ newday1.js
@@ -0,0 +1,36 @@
+console.log("hello");
+console.log("hello world".toUpperCase())
+console.log(Math.floor(Math.random()*10));
+console.log(Math.ceil(Math.random()*10));
+console.log(Math.round(Math.random()*10));
+
+console.log("  |    |  ")
+console.log("  |    |  ")
+console.log("----------")
+console.log("  |    |  ")
+console.log("  |    |  ")
+console.log("----------")
+console.log("  |    |  ")
+console.log("  |    |  ")
+
+for (let i = 0; i < 9; i++) {
+    if (i == 3 || i == 6){
+    console.log("--------------")
+    }
+    console.log("    |    |    ")
+}
+
+let x = " |      |  "
+let y = "-----------"
+
+console.log(x);
+console.log(x);
+console.log(x);
+console.log(y);
+console.log(x);
+console.log(x);
+console.log(x);
+console.log(y);
+console.log(x);
+console.log(x);
+console.log(x);
