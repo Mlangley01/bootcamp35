25f492b839ff0723b8749f8c31598ec6ecff922f
Martin <martinjlangley@gmail.com>
2022-02-18 18:15:06 +0000

    appended activities in file

--- newday2.js
+++ newday2.js
@@ -1,23 +1,37 @@
-const string = 'The revolution will not be televised.';
-console.log(string);    
+console.log("All around the world".length)
 
-const bigmouth = 'I\'ve got no right to take my place...';
-console.log(bigmouth);        
+let i =20;//create var i give it a value ""or number
+i+=2;//add 2 to the number
+console.log(i);//output
 
-const myString = '123';
-const myNum = Number(myString);
-console.log(typeof myNum);
+ let favDrink1 = "Cappucino";//create a value 
+ console.log(favDrink1);//output value
 
-const myNum2 = 123;
-const myString2 = myNum2.toString();
-console.log(typeof myString2);
+ favDrink1 = "Latte";//updates variable
+ console.log("My Favourite drink is not a"+favDrink1)//str literal allows multi format input/responses
+ 
+ 
 
 
-// const greeting = "Hello";
-// const name = "Martin";
-// console.log(greeting + ", " + name);
+let name = 'Martin';
+let age = 47;
+let favDrink = 'Cappucino';
+console.log("hello my name is " +name+ ".I am "+age+" and my favourite drink is a "+favDrink )
 
-const greeting = "Hello";
-const name = "Chris";
-console.log(`${greeting}, ${name}`);
+name = 'M Langley';
+age = 46;
+favColour = 'Purple';
+console.log("Dont Forget My name is actually "+name+". And I am only "+age+" and my favourite colour is "+favColour)
 
+// Activity two Create a program that stores what you eat today 
+//for breakfast, lunch and dinner. Log these to the console 
+
+let breakfast ='Ceral';
+let lunch = 'Salad';
+let tea = 'Pasta';
+console.log(`for breakfast I like ${breakfast}, For my lunch I like ${lunch}, and for my tea I like ${tea}`);
+//Then update the variables then output using (str literal)
+breakfast = 'Toast';
+lunch = 'Soup';
+tea = 'Steak';
+console.log(`tomorrow for breakfast i will be having ${breakfast}, Followed by${lunch} for my lunch, finished with a nice ${tea} for my Tea`);
